76e1ed7cd1bc37deb6a20d65fd634dae3356c67e
Adrián Szentkuti <51090249+SzAdrian@users.noreply.github.com>
2020-09-30 22:36:29 +0200

    added "/new" route

--- js-users/src/App.js
+++ js-users/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import AppContextProvider from "./components/Contexts/AppContext";
+import NewUser from "./components/NewUser/NewUser";
 import UserList from "./components/UserList/UserList";
 
 function App() {
   return (
-    <Router>
-      <Route exact path={["/", "/user-list"]} component={UserList} />
-    </Router>
+    <AppContextProvider>
+      <Router>
+        <Route exact path="/new" component={NewUser} />
+        <Route path={["/", "/user-list"]} component={UserList} />
+      </Router>
+    </AppContextProvider>
   );
 }
 
