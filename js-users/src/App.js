61ad256546630dc5e0b6150132e37c3024d77a19
Adrián Szentkuti <51090249+SzAdrian@users.noreply.github.com>
2020-10-01 18:28:37 +0200

    changed component name

--- js-users/src/App.js
+++ js-users/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AppContextProvider from "./components/Contexts/AppContext";
-import NewUser from "./components/NewUser/NewUser";
+import FormModal from "./components/NewUser/FormModal";
 import UserList from "./components/UserList/UserList";
 
 function App() {
   return (
     <AppContextProvider>
       <Router>
-        <Route exact path="/new" component={NewUser} />
-        <Route exact path="/edit/:uid" component={NewUser} />
+        <Route exact path="/new" component={FormModal} />
+        <Route exact path="/edit/:uid" component={FormModal} />
         <Route path={["/", "/user-list"]} component={UserList} />
       </Router>
     </AppContextProvider>
